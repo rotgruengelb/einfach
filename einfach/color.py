7a859e3ceaebba3e2152bf070bfb9e08d0b8ab8c
rotgruengelb <rotgrungelblab@gmail.com>
2023-07-09 15:49:40 +0200

    -__-

--- einfach/color.py
+++ einfach/color.py
@@ -4,11 +4,14 @@ from einfach.internals import errors
 
 def vec_to_rgba(vector: Vector):
     try:
-        if vector.components[4]: raise ValueError(errors.ERROR_VECTOR_NOT_VEC4)
-    except IndexError: pass
+        if vector.components[4]: 
+            raise ValueError(errors.ERROR_VECTOR_NOT_VEC4)
+    except IndexError: 
+        pass
     try:
         a = int(vector.components[3] * 255)
-    except IndexError: a = 255
+    except IndexError: 
+        a = 255
     return Color(
             r = int(vector.components[0] * 255),
             g = int(vector.components[1] * 255),
